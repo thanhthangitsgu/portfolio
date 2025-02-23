dc2e0a0c580f3aa420771c4d69256548e3eb5286
Thang PT - SWE <thangpt@cnv.vn>
2025-02-24 01:41:50 +0700

    Update func lang

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    Button: typeof import('primevue/button')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     Select: typeof import('primevue/select')['default']
--- src/utils/I18nHelper.ts
+++ src/utils/I18nHelper.ts
@@ -1,5 +1,5 @@
 import i18n from '../i18n.ts'
 
-export function lang(key) {
+export function lang(key: string) {
   return i18n.global.t(key);
 }
